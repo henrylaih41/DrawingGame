aeab905790722f3a0b7a99dbce71ae7352eb0b1c
henrylaih41 <henrylaih41@gmail.com>
2025-05-27 16:16:18 -0400

    minor fix

--- src/server/LiveLeaderBoard.server.lua
+++ src/server/LiveLeaderBoard.server.lua
@@ -8,24 +8,27 @@ local PlayerStore = require(ServerScriptService.modules.PlayerStore)
 local LeaderboardService = require(ServerScriptService.modules.LeaderboardService)
 local Events = ReplicatedStorage:WaitForChild("Events")
 
---------------------------------------------------------------------
+local updateLeaderboard = function(plr)
+    local playerData = PlayerStore:getPlayer(tostring(plr.UserId), true)
+    local points = playerData["TotalPoints"]
+    -- hard code threshold for now
+    if points > 14 then
+        LeaderboardService.putInMap(tostring(plr.UserId), plr.Name, points)
+    end
+end
 --------------------------------------------------------------------
 game.Players.PlayerAdded:Connect(function(plr)
     -- simple debounce: one read per join
-    task.spawn(function()
-        local playerData = PlayerStore:getPlayer(tostring(plr.UserId))
-        local points = playerData["TotalPoints"]
-        -- hard code threshold for now
-        if points > 14 then
-            LeaderboardService.putInMap(tostring(plr.UserId), plr.Name, points)
-        end
-    end)
+    task.spawn(updateLeaderboard, plr)
+end)
+
+game.Players.PlayerRemoving:Connect(function(plr)
+    task.spawn(updateLeaderboard, plr)
 end)
 
 Events.RequestTopScores.OnServerEvent:Connect(function(player)
     local playerData = PlayerStore:getPlayer(tostring(player.UserId))
     local topScores = LeaderboardService.getCachedTopScores()
-
     Events.ReceiveTopScores:FireClient(player, topScores, playerData)
 end)
 
